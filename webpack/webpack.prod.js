1edc924b0b2633259b2aeb093ed4597b8e164df8
hujin0327 <hujin0327@126.com>
2017-11-14 11:13:58 +0800

    fixed build error

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -78,23 +78,4 @@ const webpackConfig = merge(baseWebpackConfig, {
     ]
 })
 
-module.exports = function (analysis) {
-    if (analysis) {
-        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-        webpackConfig.plugins.push(
-            new BundleAnalyzerPlugin({
-                analyzerMode: 'server',
-                analyzerHost: '127.0.0.1',
-                analyzerPort: 8888,
-                reportFilename: 'report.html',
-                defaultSizes: 'parsed',
-                openAnalyzer: true,
-                generateStatsFile: false,
-                statsFilename: 'stats.json',
-                statsOptions: null,
-                logLevel: 'info'
-            })
-        )
-    }
-    return webpackConfig
-}
+module.exports = webpackConfig;
